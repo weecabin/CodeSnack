9005136b5497dfa2f84b7547975f202a5db5ca49
Weecabin <weecabin-github@yahoo.com>
2022-01-06 19:11:51 +0000

    adding cpp

---
+++ cslib/app.cpp
@@ -0,0 +1,13 @@
+// This is a basic program on C++
+//
+// Try to modify and run it and check out
+// the output in the terminal below.
+//
+// Happy coding! :-)
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
