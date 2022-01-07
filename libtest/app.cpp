3c24f4f4406f0ca485528b1dffd09f40298d31ac
Weecabin <weecabin-github@yahoo.com>
2022-01-07 21:39:44 +0000

    Added Circular buffer small test

--- libtest/app.cpp
+++ libtest/app.cpp
@@ -8,8 +8,12 @@
 #include <iostream>
 
 int main() {
+    std::cout << "Created buffer of 10 floats, then added more than 10\n";
     CircularBuffer<float> cb;
     cb.SetSize(10);
-    std::cout << "Hello, World!\n";
+    for (float i=1.1;i<20;i+=1.1)
+      cb.Push(i);
+    cb.Print();
+    
     return 0;
 }
