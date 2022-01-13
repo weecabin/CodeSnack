959dfc069bbd3f7494873c919c0d4bdce5b3667e
Weecabin <weecabin-github@yahoo.com>
2022-01-13 16:55:08 +0000

    modified to work with new Scheduler

--- libtest/app.cpp
+++ libtest/app.cpp
@@ -1,20 +1,61 @@
-// This is a basic program on C++
-//
-// Try to modify and run it and check out
-// the output in the terminal below.
-//
-// Testing
-// Happy coding! :-)
+// Verifying that I can include files from another folder
+
 #include "../cslib/CircularBuffer.h"
+#include "../cslib/Scheduler.h"
 #include <iostream>
 
+#define print(x)(std::cout<<x)
+#define println(x)(std::cout<<x<<"\n")
+
+void CircularBufferTest();
+void SchedulerTest();
+
 int main() {
-    std::cout << "Created buffer of 10 floats, then added more than 10\n";
-    CircularBuffer<float> cb;
-    cb.SetSize(10);
-    for (float i=1.1;i<20;i+=1.1)
-      cb.Push(i);
-    cb.Print();
     
+    CircularBufferTest();
+    SchedulerTest();
     return 0;
+}
+
+void task1()
+{
+  println("task1:");
+}
+
+void SchedulerTest()
+{
+  Scheduler s(5);
+  s.AddTask(new FunctionTask(task1,.5));
+  s.AddTask(new ExampleTask("t1",.5));
+  s.AddTask(new ExampleTask("t2",1));
+  s.Run(5);
+}
+
+void CircularBufferTest()
+{
+  println("Created buffer of 10 floats, then added more than 10");
+  println("Testing");
+  CircularBuffer<float> cb;
+  cb.SetSize(10);
+  for (float i=1.1;i<20;i+=1.1)
+  {
+    print("adding: ");
+    println(i);
+    cb.Push(i);
+  }
+  println("print the contents...");
+  cb.Print();
+  println("remove the 5'th element");
+  cb.Delete(5);
+  cb.Print();
+  println("insert at the 5'th");
+  cb.Insert(5,29.1);
+  cb.Print();
+  println("insert at the 9'th");
+  cb.Insert(9,39.1);
+  cb.Print();
+  println("insert at the 10'th");
+  println("nothing should happen, can't insert at the end");
+  cb.Insert(10,30.1);
+  cb.Print();
 }
