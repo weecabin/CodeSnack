7adf1a76eab40ce3e53520d544263860d7ca99da
Weecabin <weecabin-github@yahoo.com>
2022-01-14 19:19:46 +0000

    added wind, maxRudder and set precision of the printout

--- pidtesting/app.cpp
+++ pidtesting/app.cpp
@@ -7,9 +7,10 @@
 #include "../cslib/PIDCtrl.h"
 #include "../cslib/Navigation.h"
 #include <iostream>
+#include <iomanip>
 
-#define println(x)(std::cout<<x<<"\n")
-#define print(x)(std::cout<<x)
+#define println(x)(std::cout<<std::setprecision(3)<<x<<"\n")
+#define print(x)(std::cout<<std::setprecision(3)<<x)
   
 class Heading
 {
@@ -18,16 +19,24 @@ class Heading
   // rudder: Initial rudder position
   // turnRate: (degrees/sec)/degree of rudder setting
   // sampleInterval: time between samples
-  Heading(float initialValue,float initialRudder, float turnRate)
+  Heading(float initialValue,float initialRudder,float maxRudder, float turnRate, float windRate=0)
   {
     heading = initialValue;
     rudder=initialRudder;
+    this->maxRudder=maxRudder;
     this->turnRate=turnRate; // degrees per second per 1 deg of rudder
     time = 0;
+    this->windRate=windRate;
   }
   void SetRudder(float degrees)
   {
     rudder=degrees;
+    if (rudder>maxRudder && rudder<180)rudder=maxRudder;
+    else if (rudder<(360-maxRudder) && rudder>180)rudder=360-maxRudder;
+  }
+  float GetRudder()
+  {
+    return rudder;
   }
   float GetHeading(float time)
   {
@@ -35,36 +44,41 @@ class Heading
       return heading;
     float interval = time-this->time;
     this->time=time;
-    heading += rudder*turnRate*interval;
+    heading += rudder*turnRate*interval + windRate*interval;
     heading = FixHeading(heading);
     return heading;
   }
   private:
   float heading;
   float rudder;
+  float maxRudder;
   float turnRate;
   float time;
+  float windRate;
 };
 
 int main() {
-    float initialHeading=355;
+    float initialHeading=0;
     float initialRudder=0;
+    float maxRudder=30;
     float turnRate=.1;// (deg/sec)/rudderdeg
-    float target=5;
+    float target=10;
     float interval=1;
+    float windRate=.5;
     float kp=5;
     float ki=0;
     float kd=0;
   
-    PIDCtrl p(kp,ki,kd,interval,10);
-    Heading h(initialHeading,initialRudder,turnRate);
-    println("time\terror\thead");
+    PIDCtrl p(kp,ki,kd,interval,20);
+    Heading h(initialHeading,initialRudder,maxRudder,turnRate,windRate);
+    println("time\terror\trudder\thead");
     for (int i=0;i<50;i++)
     {
       float time = i*interval;
       float heading = h.GetHeading(time);
       print(time);
       print("\t");print(HeadingError(target,heading));
+      print("\t");print(h.GetRudder());
       print("\t");println(heading);
       p.NextError(-HeadingError(target,heading));
       h.SetRudder(p.Correction());
