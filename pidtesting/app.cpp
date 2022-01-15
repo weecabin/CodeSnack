0e37fa44964501df7e6322d8265880b9c42c56d8
Weecabin <weecabin-github@yahoo.com>
2022-01-15 23:06:34 +0000

    Added support for the new DeltaError in PIDCtrl

--- pidtesting/app.cpp
+++ pidtesting/app.cpp
@@ -28,6 +28,11 @@ class Heading
     time = 0;
     this->windRate=windRate;
   }
+  void Init(float initialValue,float initialRudder)
+  {
+    heading = initialValue;
+    rudder=initialRudder;
+  }
   void SetRudder(float degrees)
   {
     rudder=degrees;
@@ -60,28 +65,60 @@ class Heading
 int main() {
     float initialHeading=0;
     float initialRudder=0;
-    float maxRudder=30;
+    float maxRudder=30.0;
     float turnRate=.1;// (deg/sec)/rudderdeg
-    float target=10;
+    float target=10.0;
     float interval=1;
-    float windRate=.5;
+    float windRate=0;
     float kp=5;
-    float ki=0;
+    float ki=.9;
     float kd=0;
-  
     PIDCtrl p(kp,ki,kd,interval,20);
     Heading h(initialHeading,initialRudder,maxRudder,turnRate,windRate);
-    println("time\terror\trudder\thead");
-    for (int i=0;i<50;i++)
+    if (false)
     {
-      float time = i*interval;
-      float heading = h.GetHeading(time);
-      print(time);
-      print("\t");print(HeadingError(target,heading));
-      print("\t");print(h.GetRudder());
-      print("\t");println(heading);
-      p.NextError(-HeadingError(target,heading));
-      h.SetRudder(p.Correction());
+    for (float kp=0;kp<=5;kp+=.2)
+      for (float ki=0;ki<=5;ki+=.2)
+        for(float kd=0;kd>=-5;kd-=.2)
+        {
+          p.SetCoefficients(kp,ki,kd);
+          h.Init(initialHeading,initialRudder);
+          float heading;
+          for (int i=0;i<30;i++)
+          {
+            float time =i*interval;
+            heading = h.GetHeading(time);
+            p.NextError(-HeadingError(target,heading));
+            h.SetRudder(p.Correction());
+          }
+          float err = abs(heading-target);
+          if (p.DeltaError()<0.1 && err<0.1)
+          {
+            print(heading);print("/");print(target);print(" ");println(err);
+            print("kp,ki,kd: ");print(kp);print(",");print(ki);print(",");print(kd);
+            print(" DeltaErr: ");print(p.DeltaError());print(" heading: ");println(heading);
+          }
+        }
+      }
+      else
+      {
+        p.SetCoefficients(2.8,0,0);
+        h.Init(initialHeading,initialRudder);
+        print("Heading Target: ");println(target);
+        println("time\terror\trudder\thead");
+       for (int i=0;i<40;i++)
+       {
+        float time = i*interval;
+        float heading = h.GetHeading(time);
+        print(time);
+        print("\t");print(HeadingError(target,heading));
+        print("\t");print(h.GetRudder());
+        print("\t");println(heading);
+        p.NextError(-HeadingError(target,heading));
+        h.SetRudder(p.Correction());
+      }
+      print("Integral: ");println(p.Integral());
+      p.Print();
     }
     return 0;
 }
