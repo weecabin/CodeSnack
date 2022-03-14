4e5038646246f6572dc345ad167f260f3a0b6476
Weecabin <weecabin-github@yahoo.com>
2022-03-14 05:07:05 +0000

    playing around with an array of delegates

--- delegates/Sources/app/app.swift
+++ delegates/Sources/app/app.swift
@@ -17,42 +17,59 @@ as its delegate variable is not nil
 */
 
 class Test1{
-    init(){
-         messageInDelegate = nil
-    }
     var messageInDelegate:MessageInDelegate?
 
+  var msgInDel:[MessageInDelegate] = []
+
+  func AddDelegate(del:MessageInDelegate){
+      msgInDel.append(del)
+  }
     func Hi(){
-        guard let msgdelegate = messageInDelegate else{print("Error");return}
-        msgdelegate.messageIn(msg:"Hi to you")
+        if let msgdelegate = messageInDelegate
+      {
+         msgdelegate.messageIn(msg:"Hi to you")
+      }else
+    {
+       print("No delegate registered")
+    }
+        
+      if msgInDel.count>0{
+          for i in (0...msgInDel.count-1){
+            msgInDel[i].messageIn(msg:"Hi \(i)")
+          }
+      }
     }
 }
 
 /*
 the Delegate class conforms to the protocol and defines the function, the other class will call into
-this class must assign self to the delegate variable in the other class
+this class. must assign self to the delegate variable in the other class
 */
 class Test2:MessageInDelegate{
-    var test1:Test1
 
-  init(){
-      test1 = Test1()
-  }
   func messageIn(msg:String){
-      print(msg)
-  }
-  func sayHiGood(){
-    print("Good sayHi")
-    test1.messageInDelegate = self
-    test1.Hi()
-    test1.messageInDelegate = nil
-  }
-  func sayHiBad(){
-    print("Bad sayHi")
-    test1.Hi()
+      print("in test2 delegate: \(msg)")
   }
 }
+class Test3:MessageInDelegate{
 
+  func messageIn(msg:String){
+      print("in test3 delegate: \(msg)")
+  }
+}
+var test3 = Test3()
 var test2 = Test2()
-test2.sayHiGood()
-test2.sayHiBad()
+var test1 = Test1()
+
+print("\none to one delegate without registering...")
+test1.Hi()
+print("Now register...")
+test1.messageInDelegate = test2
+test1.Hi()
+
+print("\ntry the array delegate...")
+test1.messageInDelegate = nil
+test1.AddDelegate(del:test2)
+test1.AddDelegate(del:test3)
+test1.AddDelegate(del:test3)
+test1.Hi()
